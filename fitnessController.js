afd48359b344b54aab5f8264fced9dde97bc7024
Anteneh Negera <angias.forever@gmail.com>
2020-07-26 18:04:55 -0600

    server.js,fitnessController.js added

---
+++ fitnessController.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const db = require("../models");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  db.workout.find({}, (req, data) => {
+    res.json(data);
+  });
+});
